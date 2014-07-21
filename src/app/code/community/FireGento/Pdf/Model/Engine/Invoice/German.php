697cbf3561c3167618089fc11b636af02cfb3050
akn <a.kellmann@imi.de>
2014-07-21 17:43:12 +0200

    composer.jsonFiregento Fork added

--- src/app/code/community/FireGento/Pdf/Model/Engine/Invoice/German.php
+++ src/app/code/community/FireGento/Pdf/Model/Engine/Invoice/German.php
@@ -31,23 +31,76 @@
  * @version   $Id:$
  * @since     0.1.0
  */
-class FireGento_Pdf_Model_Engine_Invoice_German extends FireGento_Pdf_Model_Engine_Abstract
+class FireGento_Pdf_Model_Engine_Invoice_German extends FireGento_Pdf_Model_Engine_Abstract_Default
 {
 
     /**
-     * constructor to set mode to invoice
+     * Draw header for item table
+     *
+     * @param Zend_Pdf_Page $page
+     * @return void
      */
-    public function __construct()
+    protected function _drawHeader(Zend_Pdf_Page $page)
     {
-        parent::__construct();
-        $this->setMode('invoice');
+        /* Add table head */
+        $this->_setFontRegular($page, 10);
+        $page->setFillColor(new Zend_Pdf_Color_RGB(0.93, 0.92, 0.92));
+        $page->setLineColor(new Zend_Pdf_Color_GrayScale(0.5));
+        $page->setLineWidth(0.5);
+        $page->drawRectangle(25, $this->y, 570, $this->y -15);
+        $this->y -= 10;
+        $page->setFillColor(new Zend_Pdf_Color_RGB(0, 0, 0));
+
+        //columns headers
+        $lines[0][] = array(
+            'text' => Mage::helper('sales')->__('Products'),
+            'feed' => 35
+        );
+
+        $lines[0][] = array(
+            'text'  => Mage::helper('sales')->__('SKU'),
+            'feed'  => 290,
+            'align' => 'right'
+        );
+
+        $lines[0][] = array(
+            'text'  => Mage::helper('sales')->__('Qty'),
+            'feed'  => 435,
+            'align' => 'right'
+        );
+
+        $lines[0][] = array(
+            'text'  => Mage::helper('sales')->__('Price'),
+            'feed'  => 360,
+            'align' => 'right'
+        );
+
+        $lines[0][] = array(
+            'text'  => Mage::helper('sales')->__('Tax'),
+            'feed'  => 495,
+            'align' => 'right'
+        );
+
+        $lines[0][] = array(
+            'text'  => Mage::helper('sales')->__('Subtotal'),
+            'feed'  => 565,
+            'align' => 'right'
+        );
+
+        $lineBlock = array(
+            'lines'  => $lines,
+            'height' => 5
+        );
+
+        $this->drawLineBlocks($page, array($lineBlock), array('table_header' => true));
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+        $this->y -= 20;
     }
 
     /**
      * Return PDF document
      *
-     * @param  array $invoices invoices to render pdfs for
-     *
+     * @param  array $invoices
      * @return Zend_Pdf
      */
     public function getPdf($invoices = array())
@@ -57,160 +110,67 @@ class FireGento_Pdf_Model_Engine_Invoice_German extends FireGento_Pdf_Model_Engi
 
         $pdf = new Zend_Pdf();
         $this->_setPdf($pdf);
-
-        // pagecounter is 0 at the beginning, because it is incremented in newPage()
-        $this->pagecounter = 0;
+        $style = new Zend_Pdf_Style();
+        $this->_setFontBold($style, 10);
 
         foreach ($invoices as $invoice) {
             if ($invoice->getStoreId()) {
                 Mage::app()->getLocale()->emulate($invoice->getStoreId());
                 Mage::app()->setCurrentStore($invoice->getStoreId());
             }
+            $page  = $this->newPage();
             $order = $invoice->getOrder();
-            $this->setOrder($order);
-
-            $page = $this->newPage();
-
-            $this->insertAddressesAndHeader($page, $invoice, $order);
-
-            $this->_setFontRegular($page, 9);
-            $this->insertTableHeader($page);
-
-            $this->y -= 20;
-
-            $position = 0;
-
-            foreach ($invoice->getAllItems() as $item) {
+            /* Add image */
+            $this->insertLogo($page, $invoice->getStore());
+            /* Add address */
+            $this->insertAddress($page, $invoice->getStore());
+            /* Add head */
+            $this->insertOrder(
+                $page,
+                $order,
+                Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID, $order->getStoreId())
+            );
+            /* Add document text and number */
+            $this->insertDocumentNumber(
+                $page,
+                Mage::helper('sales')->__('Invoice # ') . $invoice->getIncrementId()
+            );
+            /* Add table */
+            $this->_drawHeader($page);
+            /* Add body */
+            foreach ($invoice->getAllItems() as $item){
                 if ($item->getOrderItem()->getParentItem()) {
                     continue;
                 }
-
-                $showFooter = Mage::getStoreConfig('sales_pdf/firegento_pdf/show_footer');
-                if ($this->y < 50 || ($showFooter == 1 && $this->y < 100)) {
-                    $page = $this->newPage(array());
-                }
-
-                $position++;
-                $page = $this->_drawItem($item, $page, $order, $position);
+                /* Draw item */
+                $this->_drawItem($item, $page, $order);
+                $page = end($pdf->pages);
             }
-
-            /* add line after items */
-            $page->drawLine($this->margin['left'], $this->y + 5, $this->margin['right'], $this->y + 5);
-
-            /* add totals */
-            $page = $this->insertTotals($page, $invoice);
-
-            /* add note */
-            $page = $this->_insertNote($page, $order, $invoice);
-
-            // Add footer
-            $this->_addFooter($page, $invoice->getStore());
+            /* Add totals */
+            $this->insertTotals($page, $invoice);
+            if ($invoice->getStoreId()) {
+                Mage::app()->getLocale()->revert();
+            }
         }
-
         $this->_afterGetPdf();
-
         return $pdf;
     }
 
     /**
-     * Insert Table Header for Items
+     * Create new page and assign to PDF object
      *
-     * @param  Zend_Pdf_Page $page current page object of Zend_PDF
-     *
-     * @return void
+     * @param  array $settings
+     * @return Zend_Pdf_Page
      */
-    protected function insertTableHeader(&$page)
+    public function newPage(array $settings = array())
     {
-        $page->setFillColor($this->colors['grey1']);
-        $page->setLineColor($this->colors['grey1']);
-        $page->setLineWidth(1);
-        $page->drawRectangle($this->margin['left'], $this->y, $this->margin['right'], $this->y - 15);
-
-        $page->setFillColor($this->colors['black']);
-        $font = $this->_setFontRegular($page, 9);
-
-        $this->y -= 11;
-        $page->drawText(Mage::helper('firegento_pdf')->__('Pos'), $this->margin['left'] + 3, $this->y, $this->encoding);
-        $page->drawText(
-            Mage::helper('firegento_pdf')->__('No.'), $this->margin['left'] + 25, $this->y, $this->encoding
-        );
-        $page->drawText(
-            Mage::helper('firegento_pdf')->__('Description'), $this->margin['left'] + 130, $this->y, $this->encoding
-        );
-
-        $columns = array();
-        $columns['price'] = array(
-            'label'  => Mage::helper('firegento_pdf')->__('Price'),
-            '_width' => 60
-        );
-        $columns['price_incl_tax'] = array(
-            'label'  => Mage::helper('firegento_pdf')->__('Price (incl. tax)'),
-            '_width' => 60
-        );
-        $columns['qty'] = array(
-            'label'  => Mage::helper('firegento_pdf')->__('Qty'),
-            '_width' => 40
-        );
-        $columns['tax'] = array(
-            'label'  => Mage::helper('firegento_pdf')->__('Tax'),
-            '_width' => 50
-        );
-        $columns['tax_rate'] = array(
-            'label'  => Mage::helper('firegento_pdf')->__('Tax rate'),
-            '_width' => 50
-        );
-        $columns['subtotal'] = array(
-            'label'  => Mage::helper('firegento_pdf')->__('Total'),
-            '_width' => 50
-        );
-        $columns['subtotal_incl_tax'] = array(
-            'label'  => Mage::helper('firegento_pdf')->__('Total (incl. tax)'),
-            '_width' => 70
-        );
-        // draw price, tax, and subtotal in specified order
-        $columnsOrder = explode(',', Mage::getStoreConfig('sales_pdf/invoice/item_price_column_order'));
-        // draw starting from right
-        $columnsOrder = array_reverse($columnsOrder);
-        $columnOffset = 0;
-        foreach ($columnsOrder as $columnName) {
-            $columnName = trim($columnName);
-            if (array_key_exists($columnName, $columns)) {
-                $column = $columns[$columnName];
-                $labelWidth = $this->widthForStringUsingFontSize($column['label'], $font, 9);
-                $page->drawText(
-                    $column['label'],
-                    $this->margin['right'] - $columnOffset - $labelWidth,
-                    $this->y,
-                    $this->encoding
-                );
-                $columnOffset += $column['_width'];
-            }
+        /* Add new table head */
+        $page = $this->_getPdf()->newPage(Zend_Pdf_Page::SIZE_A4);
+        $this->_getPdf()->pages[] = $page;
+        $this->y = 800;
+        if (!empty($settings['table_header'])) {
+            $this->_drawHeader($page);
         }
+        return $page;
     }
-
-    /**
-     * Initialize renderer process
-     *
-     * @param  string $type renderer type to be initialized
-     *
-     * @return void
-     */
-    protected function _initRenderer($type)
-    {
-        parent::_initRenderer($type);
-
-        $this->_renderers['default'] = array(
-            'model'    => 'firegento_pdf/items_default',
-            'renderer' => null
-        );
-        $this->_renderers['grouped'] = array(
-            'model'    => 'firegento_pdf/items_grouped',
-            'renderer' => null
-        );
-        $this->_renderers['bundle'] = array(
-            'model'    => 'firegento_pdf/items_bundle',
-            'renderer' => null
-        );
-    }
-
 }
